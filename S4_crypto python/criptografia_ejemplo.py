1bf141da09db865793795af2ccd2920a4cd91d7d
alvarado0925 <alvarado0925@gmail.com>
2024-02-07 00:16:50 -0500

    Ajustes menores

--- S4_crypto python/criptografia_ejemplo.py
+++ S4_crypto python/criptografia_ejemplo.py
@@ -3,20 +3,22 @@ from cryptography.fernet import Fernet
 
 # Generar una clave
 key = Fernet.generate_key()
-#Crear una instancia de Fernet
-cipher_suite = Fernet(key) 
+
+# Crear una instancia de Fernet
+cipher_suite = Fernet(key)
 
 # Cifrar un mensaje
-#Se define un mensaje en forma de bytes b
-message = b"mensaje secreto estudiante UNIMINUTO"
-#El mensaje cifrado se almacena en la variable encrypted_message
+# Se define un mensaje en forma de bytes b
+message = b"Este es un mensaje secreto"
+
+# El mensaje cifrado se almacena en la variable encrypted_message
 encrypted_message = cipher_suite.encrypt(message)
 
 # Descifrar el mensaje
 # Se utiliza la misma instancia de Fernet para descifrar el mensaje cifrado
 decrypted_message = cipher_suite.decrypt(encrypted_message)
 
-#Imprimir los resultados
+# Imprimir los resultados
 print("Mensaje original:", message)
 print("Mensaje cifrado:", encrypted_message)
 print("Mensaje descifrado:", decrypted_message)
